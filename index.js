7fa4ccf03995d487d547919e1f8db9693b7ac4c7
personal <yordirguevara@gmail.com>
2025-03-08 00:24:18 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+
+// DB Config
+require('./database/config').dbConnection();
+
+
+// App de Express
+const app = express();
+app.use(cors())
+
+// Lectura y parseo del Body
+app.use( express.json() );
+
+// Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+// Mis Rutas
+app.use( '/api/login', require('./routes/auth') );
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
+});
---
+++ models/usuario.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+    apellido: {
+        type: String,
+        default: ""
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    online: {
+        type: Boolean,
+        default: false
+    },
+    type: {
+        type: String,
+        default: 'USER'
+    },
+    telefono: {
+        type: String,
+        default: ""
+    },
+    status: {
+        type: String,
+        default: "ACTIVE"
+    },
+
+});
+
+UsuarioSchema.method('toJSON', function() {
+    const { __v, _id, password, ...object } = this.toObject();
+    object.uid = _id;
+    return object;
+})
+
+
+
+module.exports = model('Usuario', UsuarioSchema );
