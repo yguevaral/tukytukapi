54e97415b10018dcb7ea2219bb35e27f3e2dd4c3
personal <yordirguevara@gmail.com>
2025-03-08 01:26:36 -0600

    routes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
+require('dotenv').config();
 
 // DB Config
 require('./database/config').dbConnection();
@@ -9,22 +8,30 @@ require('./database/config').dbConnection();
 
 // App de Express
 const app = express();
-app.use(cors())
 
 // Lectura y parseo del Body
 app.use( express.json() );
+
 
 // Node Server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
+
+
+
 // Path público
 const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath ) );
 
+
+
 // Mis Rutas
 app.use( '/api/login', require('./routes/auth') );
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/mensajes', require('./routes/mensajes') );
+
 
 
 server.listen( process.env.PORT, ( err ) => {
